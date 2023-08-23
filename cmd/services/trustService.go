ce4cea1302fdd4795aa16b1bc385ded16d889d10
Burak Berk <burakberkkeskin@gmail.com>
2023-08-23 18:29:37 +0300

    added trust cert for macOS

--- cmd/services/trustService.go
+++ cmd/services/trustService.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
+	"os/user"
 	"runtime"
 )
 
@@ -12,7 +14,10 @@ func TrustCrt(crtPath string) {
 	if isLinux() {
 		trustCrtOnLinux(&crtPath)
 	} else if isMacos() {
-		trustCrtOnMacos(&crtPath)
+		err := trustCrtOnMacos(&crtPath)
+		if err != nil {
+			log.Fatal("Error while trusting cert: ", err)
+		}
 	} else {
 		fmt.Println("Unknown OS. Can not trust the cert.")
 	}
@@ -36,13 +41,37 @@ func trustCrtOnLinux(crtPath *string) {
 
 func trustCrtOnMacos(crtPath *string) error {
 	fmt.Println(*crtPath, "is being trusted on MacOS...")
-	isCrtTrusted := exec.Command("security", "verify-cert", "-c", *crtPath)
-	err := isCrtTrusted.Run()
+	isCrtTrustedCommand := exec.Command("security", "verify-cert", "-c", *crtPath)
+	err := isCrtTrustedCommand.Run()
 	if err == nil {
-		fmt.Println(*crtPath, "is already trusted")
+		fmt.Println(*crtPath, "is already found on keychain")
 		return nil
 	}
-	fmt.Println(*crtPath, "is being trusted on keychain")
+	fmt.Println(*crtPath, "will be trusted on keychain")
+	sudoPermission := hasSudoPermissions()
+	if !sudoPermission {
+		return fmt.Errorf("you don't have sudo permissions to add cert to keychain")
+	}
+
+	fmt.Println("Sudo permission: ", sudoPermission)
+
+	trustCrtCommand := exec.Command("security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", *crtPath)
+	output, err := trustCrtCommand.CombinedOutput()
+	if err != nil {
+		fmt.Println("Command output:", string(output))
+		return fmt.Errorf("failed to add cert to keychain: %w", err)
+
+	}
+
+	fmt.Println(*crtPath, "has been added to keychain successfully.")
 	return nil
 
+}
+
+func hasSudoPermissions() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("[hasSudoPermissions] Unable to get current user: %s", err)
+	}
+	return currentUser.Username == "root"
 }
